df3dccb688968c0b02f171267526b7a34a9dbebf
Jordan Skomal <jskomal@gmail.com>
2022-05-12 13:50:41 -0600

    Add main title and placeholder input

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css'
 function App() {
   return (
     <div className='App'>
-      <h1>hello</h1>
+      <h1 className='main-title'>your next Shopify venture</h1>
+      <input type='text' />
     </div>
   )
 }
