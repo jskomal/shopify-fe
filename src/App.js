b03e1688bd29f0602fdbcb6c51b1917e08e37882
Jordan Skomal <jskomal@gmail.com>
2022-05-12 15:37:30 -0600

    Begin implementing POST req

--- src/App.js
+++ src/App.js
@@ -9,7 +9,26 @@ const App = () => {
     setTextInput(e.target.value)
   }
 
-  const handleSubmit = (e) => {}
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const dataToSend = {
+      prompt: textInput,
+      temperature: 0.5,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0
+    }
+
+    fetch('https://api.openai.com/v1/engines/text-curie-001/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+      },
+      body: JSON.stringify(dataToSend)
+    })
+  }
 
   return (
     <div className='App'>
